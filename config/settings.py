28b71fae40ca8ea905e78db73bde687e0d97ada9
francescawaters <francescawaters26@outlook.com>
2025-03-06 13:58:02 +0000

    Update ALLOWED_HOSTS for Heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://cons-events-app-186c83f9afb9.herokuapp.com/', '127.0.0.1' ]
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://*.herokuapp.com']
 
