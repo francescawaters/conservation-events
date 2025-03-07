ad0500a5cb5b38aafea7ed6a38482e329e62ded4
francescawaters <francescawaters26@outlook.com>
2025-03-07 08:20:38 +0000

    Add event detail page with URL routing and template

--- events/views.py
+++ events/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Event
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -22,3 +23,27 @@ class EventList(generic.ListView):
     queryset = Event.objects.filter(status=1)
     template_name = "index.html" 
     paginate_by = 6
+
+
+def event_detail(request, slug):
+    """
+    Display an individual :model:`events.Event`.
+
+    **Context**
+
+    ``events``
+        An instance of :model:`events.Event`.
+
+    **Template:**
+
+    :template:`events/event_detail.html`
+    """
+
+    queryset = Event.objects.filter(status=1)
+    event = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "events/event_detail.html",
+        {"event": event},
+    )
