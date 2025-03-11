9063f6def3b071b7ffd281a0d622e9ff3f5794d4
francescawaters <francescawaters26@outlook.com>
2025-03-11 11:00:40 +0000

    Add delete functionality for comments with confirmation modal

--- events/views.py
+++ events/views.py
@@ -95,3 +95,21 @@ def comment_edit(request, slug, comment_id):
                                  'Error updating comment!')
 
     return HttpResponseRedirect(reverse('event_detail', args=[slug]))
+
+
+def comment_delete(request, slug, comment_id):
+    """
+    view to delete comment
+    """
+    queryset = Event.objects.filter(status=1)
+    event = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+
+    if comment.author == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+    else:
+        messages.add_message(request, messages.ERROR, 'You can only delete'
+                                                      ' your own comments!')
+
+    return HttpResponseRedirect(reverse('event_detail', args=[slug]))
