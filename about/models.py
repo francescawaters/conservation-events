8dc43739f1cfa3b627d90291743ed63b0d75e61a
francescawaters <francescawaters26@outlook.com>
2025-03-12 08:25:24 +0000

    Add contact form and model, update about page to handle submissions

--- about/models.py
+++ about/models.py
@@ -13,3 +13,16 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Contact(models.Model):
+    """
+    A model to store contact messages.
+    """
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Message from {self.name}"
--- about/views.py
+++ about/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .forms import ContactForm
 
 # Create your views here.
 
 
 def about_page(request):
     """
-    Renders the about page.
+    Renders the about page with contact form.
     """
-    about = About.objects.all().order_by('-updated').first()
+    if request.method == 'POST':
+        contact = ContactForm(data=request.POST)
+        if contact.is_valid():
+            contact.save()
+            messages.add_message(
+                request, messages.SUCCESS, 'Message sent successfully.'
+            )
+        else:
+            messages.add_message(
+                request, messages.ERROR, 'Message not sent. Please try again.'
+            )
 
-    return render(request, 'about.html', {'about': about})
+    about = About.objects.all().order_by('-updated').first()
+    contact = ContactForm()
+
+    return render(
+        request, 'about.html',
+        {
+            'about': about,
+            'contact': contact,
+         },
+    )
